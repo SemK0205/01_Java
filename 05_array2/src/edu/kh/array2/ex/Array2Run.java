45f1792f0bb45bd880f79d9e9444b5e4dee55896
SemK0205 <s413625@gamil.com>
2025-02-19 17:16:02 +0900

    25.02.19

---
+++ 05_array2/src/edu/kh/array2/ex/Array2Run.java
@@ -0,0 +1,12 @@
+package edu.kh.array2.ex;
+
+public class Array2Run {
+	public static void main(String[] args) {
+		
+		Array2Example arrayEx = new Array2Example();
+		//arrayEx.ex1();
+		//arrayEx.ex2();
+		//arrayEx.ex3();
+	}
+
+}
