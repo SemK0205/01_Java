8606ff1799547212b927380f2abeafc527ef5fac
SemK0205 <s413625@gmail.com>
2025-02-27 17:09:21 +0900

    25.02.27

---
+++ 10_exception/src/edu/kh/exception/model/service/UserException.java
@@ -0,0 +1,16 @@
+package edu.kh.exception.model.service;
+
+// 사용자 정의 예외 클래스
+public class UserException extends Exception{
+
+	public UserException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserException(String message) {
+		super(message);
+	}
+	
+	
+	
+}
